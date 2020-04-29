a48834923240ccb3f9c7d77c14545d71cd755347
John Leonardo <johnleonardo@csus.edu>
2020-04-28 23:26:18 -0700

    server working

---
+++ lib/server.ts
@@ -0,0 +1,10 @@
+// get app
+import app from "./app";
+
+// port to listen on
+const PORT: number = 3000;
+
+// listen on port
+app.listen(PORT, () => {
+    console.log("Express server listening on port " + PORT);
+});
