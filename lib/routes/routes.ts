84d44219513e3040ee859d3f28c67ce72d8691cb
John Leonardo <johnleonardo@csus.edu>
2020-04-28 23:45:55 -0700

    /user routes

--- lib/routes/routes.ts
+++ lib/routes/routes.ts
@@ -7,9 +7,33 @@ export class Routes {
     public routes(app): void {
         // base route
         app.route('/').get((req: Request, res: Response) => {
+            // send response
             res.status(200).send({
                 message: 'Hello, world!',
             });
         });
+
+        // user routes
+        app.route('/user')
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'GET to /users',
+                });
+            })
+            .post((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'POST to /users',
+                });
+            })
+            .put((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'PUT to /users',
+                });
+            })
+            .delete((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'DELETE to /users',
+                });
+            });
     }
 }
